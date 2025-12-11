b6e904565d3efd479419dab04061d4379c8a4509
mariogiampieri <mariogiampieri@gmail.com>
2025-12-11 12:16:58 -0500

    update for pip install

--- cli/src/webmap_archiver/__init__.py
+++ cli/src/webmap_archiver/__init__.py
@@ -1,10 +1,15 @@
 """
 WebMap Archiver - Archive web maps as self-contained packages.
+
+Usage:
+    from webmap_archiver import create_archive_from_bundle, ArchiveResult
+
+    result = create_archive_from_bundle(bundle_dict, Path("output.zip"))
 """
 
 __version__ = "0.2.0"
 
-# Public API
+# Public API exports
 from .api import (
     create_archive_from_bundle,
     create_archive_from_har,
@@ -15,21 +20,20 @@ from .api import (
     InspectionResult,
 )
 
-# Exceptions
 from .capture.parser import CaptureValidationError
 
 __all__ = [
-    # Functions
+    # Version
+    "__version__",
+    # Main functions
     "create_archive_from_bundle",
     "create_archive_from_har",
     "inspect_bundle",
     "normalize_bundle",
-    # Data classes
+    # Result types
     "ArchiveResult",
     "TileSourceResult",
     "InspectionResult",
     # Exceptions
     "CaptureValidationError",
-    # Metadata
-    "__version__",
 ]
